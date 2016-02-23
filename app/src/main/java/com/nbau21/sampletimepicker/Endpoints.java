1a17e2253eecd43852ec8cf11e802b9751ae3234
Noel Bautista <noelbautista91@gmail.com>
2016-02-22 22:10:50 -0800

    added endpoints class for firebase constants

---
+++ app/src/main/java/com/nbau21/sampletimepicker/Endpoints.java
@@ -0,0 +1,9 @@
+package com.nbau21.sampletimepicker;
+
+/**
+ * Created by nbau21 on 2/21/16.
+ */
+public class Endpoints {
+    public final static String FirebaseEndpoint = "https://timepickersample.firebaseio.com/";
+    public final static String TimePicker = "time_picker";
+}
